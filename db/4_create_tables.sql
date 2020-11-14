50703441a326d64323b18367cbf6d50ff7ce10d4
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-11-14 17:59:16 +0000

    Init

---
+++ db/4_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  caeschema
+-- Automatically generated sql script for the service 4, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table sd.
+--
+CREATE TABLE caeschema.sd (
+  sdc Integer ,
+CONSTRAINT sdc_PK PRIMARY KEY (sdc)
+ 
+);
+
+
+
